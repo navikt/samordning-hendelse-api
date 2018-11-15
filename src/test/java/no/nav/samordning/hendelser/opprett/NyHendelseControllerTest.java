a7ef90a2c11d9f31f895088fb5740305565503c3
Magnus Austrheim <magnus.austrheim@nav.no>
2018-11-15 08:20:12 +0100

    Renamed controller test to resolve name clash

--- src/test/java/no/nav/samordning/hendelser/opprett/NyHendelseControllerTest.java
+++ src/test/java/no/nav/samordning/hendelser/opprett/NyHendelseControllerTest.java
@@ -15,7 +15,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(NyHendelseController.class)
-public class NyHendelseController {
+public class NyHendelseControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
