32aef29dbd60720d50f2481055a86365b0f8f82d
Magnus Austrheim <Magnus.Austrheim@nav.no>
2018-12-05 10:20:12 +0100

    Added admin user with basic auth

--- src/main/java/no/nav/samordning/hendelser/security/WebSecurityConfig.java
+++ src/main/java/no/nav/samordning/hendelser/security/WebSecurityConfig.java
@@ -4,22 +4,30 @@ import no.nav.samordning.hendelser.security.jwt.JwtAuthenticationEntryPoint;
 import no.nav.samordning.hendelser.security.jwt.JwtAuthenticationFilter;
 import no.nav.samordning.hendelser.security.jwt.JwtAuthenticationProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Value("${SRV_USERNAME}")
+    private String username;
+
+    @Value("${SRV_PASSWORD}")
+    private String password;
+
     private final JwtAuthenticationFilter jwtAuthFilter;
-
     private final JwtAuthenticationProvider jwtAuthProvider;
-
     private final JwtAuthenticationEntryPoint jwtAuthEntryPoint;
 
     @Autowired
@@ -39,11 +47,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .exceptionHandling()
                 .authenticationEntryPoint(jwtAuthEntryPoint)
                 .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .httpBasic();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(jwtAuthProvider);
+        auth.inMemoryAuthentication()
+                .withUser(username)
+                .password(passwordEncoder().encode(password))
+                .roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
---
+++ src/test/java/no/nav/samordning/hendelser/authentication/BasicAuthenticationTests.java
@@ -0,0 +1,44 @@
+package no.nav.samordning.hendelser.authentication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@WebAppConfiguration
+public class BasicAuthenticationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Value("${SRV_USERNAME}")
+    private String username;
+
+    @Value("${SRV_PASSWORD}")
+    private String password;
+
+    @Test
+    public void correct_credentials_authorized() throws Exception {
+        this.mockMvc.perform(get("/hendelser").with(httpBasic(username, password))
+                .param("side", "1")).andDo(print()).andExpect(status().isOk());
+    }
+
+    @Test
+    public void wrong_credentials_unauthorized() throws Exception {
+        this.mockMvc.perform(get("/hendelser").with(httpBasic("user", "..."))
+                .param("side", "1")).andDo(print()).andExpect(status().isUnauthorized());
+    }
+}
