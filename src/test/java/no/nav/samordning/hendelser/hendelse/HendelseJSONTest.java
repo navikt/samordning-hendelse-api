c78f43f4bcbbe3f16266b360843be5cf91b67d85
Knutsen, Martin <Martin.Knutsen@nav.no>
2018-12-20 10:55:38 +0100

    Converted JSON strings to lists to fix ObjectToJSON test.

--- src/test/java/no/nav/samordning/hendelser/hendelse/HendelseJSONTest.java
+++ src/test/java/no/nav/samordning/hendelser/hendelse/HendelseJSONTest.java
@@ -1,15 +1,15 @@
 package no.nav.samordning.hendelser.hendelse;
 
+import com.jayway.jsonpath.JsonPath;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
-import javax.json.bind.JsonbConfig;
-import javax.json.bind.config.PropertyOrderStrategy;
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.hamcrest.beans.SamePropertyValuesAs.samePropertyValuesAs;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 public class HendelseJSONTest {
@@ -39,13 +39,15 @@ public class HendelseJSONTest {
         hendelse.setFom(LocalDate.of(2020, 01, 01));
         hendelse.setTom(LocalDate.of(2025, 01, 01));
 
-        JsonbConfig config = new JsonbConfig()
-                .withPropertyOrderStrategy(PropertyOrderStrategy.ANY);
-
-        Jsonb jsonb = JsonbBuilder.create(config);
+        Jsonb jsonb = JsonbBuilder.create();
         String result = jsonb.toJson(hendelse);
 
         String excpected = "{\"ytelsesType\":\"AAP\",\"identifikator\":\"12345678901\",\"vedtakId\":\"ABC123\",\"fom\":\"2020-01-01\",\"tom\":\"2025-01-01\"}";
-        assertEquals(excpected, result);
+
+        List<String> excpectedList = JsonPath.read(excpected, "$.*");
+        List<String> resultList = JsonPath.read(result, "$.*");
+
+        assertThat((excpectedList),
+                Matchers.containsInAnyOrder(resultList.toArray()));
     }
 }
