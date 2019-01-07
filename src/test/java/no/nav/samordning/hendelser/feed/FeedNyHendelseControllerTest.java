68b0213d13685ed60eb1d7eb07e782ea5a368aa0
Knutsen, Martin <Martin.Knutsen@nav.no>
2019-01-07 13:02:12 +0100

    Changed .andExcpect and added .with(user.. in greetingsShouldReturnMessageFromService test. Also removed Database Mockbean, .whens and WebMvcTest annotation.

--- src/test/java/no/nav/samordning/hendelser/feed/FeedNyHendelseControllerTest.java
+++ src/test/java/no/nav/samordning/hendelser/feed/FeedNyHendelseControllerTest.java
@@ -1,37 +1,36 @@
 package no.nav.samordning.hendelser.feed;
 
-import no.nav.samordning.hendelser.hendelse.Database;
 import no.nav.samordning.hendelser.hendelse.Hendelse;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.support.TestPropertySourceUtils;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.MountableFile;
 
-import java.util.List;
-
 import static org.hamcrest.Matchers.containsString;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
-
 @RunWith(SpringRunner.class)
 @ContextConfiguration(initializers = FeedNyHendelseControllerTest.Initializer.class)
-@WebMvcTest(value = FeedController.class, secure = false)
+@SpringBootTest
+@AutoConfigureMockMvc
+@WebAppConfiguration
 public class FeedNyHendelseControllerTest {
 
     @ClassRule
@@ -53,41 +52,39 @@ public class FeedNyHendelseControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    private Database service;
-
     @Test
     public void greetingShouldReturnMessageFromService() throws Exception {
-        var hendelse = new Hendelse();
-        hendelse.setVedtakId("1234");
-
-        when(service.fetchAll(any(), any())).thenReturn(List.of(hendelse));
-
-        this.mockMvc.perform(get("/hendelser").param("side", "1")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("{\"hendelser\":[{\"ytelsesType\":null,\"identifikator\":null,\"vedtakId\":\"1234\",\"fom\":null,\"tom\":null}]}")));
+        this.mockMvc.perform(get("/hendelser")
+                .with(user("srvTest"))
+                .param("side", "1"))
+                .andDo(print()).andExpect(status()
+                .isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.hendelser[0].fom").value("2020-01-01"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.hendelser[0].ytelsesType").value("AAP"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.hendelser[0].identifikator").value("12345678901"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.hendelser[0].vedtakId").value("ABC123"));
     }
-
 
     @Test
     public void serviceShouldRequirePageParameter() throws Exception {
         var hendelse = new Hendelse();
         hendelse.setVedtakId("1234");
 
-        when(service.fetchAll(any(), any())).thenReturn(List.of(hendelse));
-
         this.mockMvc.perform(get("/hendelser")).andDo(print()).andExpect(status().is4xxClientError());
     }
-
 
     @Test
     public void serviceShouldntAcceptTooLargeRequests() throws Exception {
         var hendelse = new Hendelse();
         hendelse.setVedtakId("1234");
 
-        when(service.fetchAll(any(), any())).thenReturn(List.of(hendelse));
-
-        this.mockMvc.perform(get("/hendelser").param("side", "1").param("antall", "10001")).andDo(print()).andExpect(status().is4xxClientError())
+        this.mockMvc.perform(get("/hendelser")
+                .with(user("srvTest"))
+                .param("side", "1")
+                .param("antall", "10001"))
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
                 .andExpect(content().string(containsString("Man kan ikke be om flere enn 10000 hendelser.")));
     }
-
 }
