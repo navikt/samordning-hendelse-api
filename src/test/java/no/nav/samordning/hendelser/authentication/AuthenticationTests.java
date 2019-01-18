474c0ac3e6e2f0d18a0da4c12644c4e058ada36a
magnuvau <Magnus.Austrheim@nav.no>
2019-01-18 16:00:34 +0100

    Mocked jwks for tests

--- src/test/java/no/nav/samordning/hendelser/authentication/AuthenticationTests.java
+++ src/test/java/no/nav/samordning/hendelser/authentication/AuthenticationTests.java
@@ -1,8 +1,12 @@
 package no.nav.samordning.hendelser.authentication;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockserver.integration.ClientAndServer;
+import org.mockserver.model.Header;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,6 +20,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.MountableFile;
 
+import java.io.File;
+import java.util.Scanner;
+
+import static org.mockserver.integration.ClientAndServer.startClientAndServer;
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -43,21 +53,46 @@ public class AuthenticationTests {
         }
     }
 
+    private static ClientAndServer mockServer;
+    private static String jwtValidToken, jwtInvalidToken;
+
+    @BeforeClass
+    public static void startServer() throws Exception {
+        var scanner = new Scanner(new File("src/test/resources", "test_jwk.json"));
+
+        var mockJwkBody = "";
+        while (scanner.hasNext()) {
+            mockJwkBody += scanner.next();
+        }
+
+        scanner.close();
+
+        jwtValidToken = new Scanner(new File("src/test/resources", "test_valid_token.txt")).next();
+        jwtInvalidToken = new Scanner(new File("src/test/resources", "test_invalid_token.txt")).next();
+
+        mockServer = startClientAndServer(1080);
+        mockServer.when(request().withMethod("GET").withPath("/test-oidc-provider/jwk")).respond(
+                response().withHeaders(new Header("Content-Type", "application/json; charset=utf-8"))
+                .withBody(mockJwkBody));
+    }
+
+    @AfterClass
+    public static void stopServer() {
+        mockServer.stop();
+    }
+
     @Autowired
     private MockMvc mockMvc;
 
-    private final String jwtToken = "eyJraWQiOiJtcVQ1QTNMT1NJSGJwS3JzY2IzRUhHcnItV0lGUmZMZGFxWl81SjlHUjlzIiwiYWxnIjoiUlMyNTYifQ.eyJhdWQiOiJvaWRjX25hdl9wb3J0YWxfdGVzdGtvbnN1bWVudCIsInNjb3BlIjoibmF2OnRlc3RhcGkyIG5hdjp0ZXN0YXBpIiwiaXNzIjoiaHR0cHM6XC9cL29pZGMtdmVyMi5kaWZpLm5vXC9pZHBvcnRlbi1vaWRjLXByb3ZpZGVyXC8iLCJ0b2tlbl90eXBlIjoiQmVhcmVyIiwiZXhwIjoxNTQ3NzI4NzQ1LCJpYXQiOjE1NDc3MjgzODUsImNsaWVudF9vcmdubyI6Ijg4OTY0MDc4MiIsImp0aSI6IlJsOEstMU9qeUFuVkN6VjJ4MGxpdXBiMW41bFJpWlZ6aXVkc1YtQ1doNWM9In0.PpMTcpuPzPVMK7U72Tcp_WKxAyRy6v-fAvo6JbtRkuk55Va6hs2LWrDQlqirZCuXa_NUNGb3DHi0JzLnKWA51VPCndzCbed9J35kzq5OggnNJu7kL2KkxsV2CF6PcB9Fw6dnGtHu1jPbT0oMwRCLyuRfMpJc2WqB0ZvzFpgelhDZLbr6nobCtVCN4aYxzEgCoqLN3OypUnakBPIkedZ9JyaLefcSJUIINcL4yVg8_8_1R5DtQViXmL_DQvOXr8xkxxQKUzOcLSaEFUkuPkLslBBLTwRDp60uW3XWIZpReuub8mShLVoeOsYczLiL8EPhoeXtkwUqnSv1cjGacvVy2A";
-
-//    @Test
-//    public void test_correct_credentials_authenticated() throws Exception {
-//
-//        this.mockMvc.perform(get("/hendelser").header("Authorization", jwtToken)
-//                .param("side", "1")).andDo(print()).andExpect(status().isOk());
-//    }
+    @Test
+    public void test_correct_credentials_authenticated() throws Exception {
+        mockMvc.perform(get("/hendelser").header("Authorization", jwtValidToken)
+                .param("side", "1")).andDo(print()).andExpect(status().isOk());
+    }
 
     @Test
     public void test_wrong_credentials_unauthenticated() throws Exception {
-        mockMvc.perform(get("/hendelser").header("Authorization", jwtToken))
-                .andDo(print()).andExpect(status().isUnauthorized());
+        mockMvc.perform(get("/hendelser").header("Authorization", jwtInvalidToken)
+                .param("side", "1")).andDo(print()).andExpect(status().isUnauthorized());
     }
 }
