d5b0cc4db62494e88551584ab70ead75342a8d94
magnuvau <Magnus.Austrheim@nav.no>
2019-04-11 09:13:34 +0200

    Refactoring and deleted legacy insert functionality

--- src/main/java/no/nav/samordning/hendelser/feed/FeedController.java
+++ src/main/java/no/nav/samordning/hendelser/feed/FeedController.java
@@ -3,26 +3,18 @@ package no.nav.samordning.hendelser.feed;
 import io.micrometer.core.annotation.Timed;
 import no.nav.samordning.hendelser.hendelse.Database;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import java.time.LocalDate;
 import java.util.stream.Collectors;
 
 
 @RestController
 public class FeedController {
-    private static final Integer MAX_ANTALL = 10000;
-    private static final String MIN_DATE_LOCALDATE = "1814-05-17";
-    private static final String MAX_DATE_LOCALDATE = "2100-01-01";
-    private static final LocalDate MIN_DATE = LocalDate.parse(MIN_DATE_LOCALDATE);
-    private static final LocalDate MAX_DATE = LocalDate.parse(MAX_DATE_LOCALDATE);
-
-    private static final String DEFAULT_SIDE = "0";
-    private static final String DEFAULT_ANTALL = "10000";
 
     private Database database;
 
@@ -31,17 +23,23 @@ public class FeedController {
         this.database = database;
     }
 
+    @Value("${FEED_MAX_ANTALL}")
+    private int MAX_ANTALL;
+
+    @Value("${FEED_DEFAULT_SIDE}")
+    private int DEFAULT_SIDE;
+
+    @Value("${FEED_DEFAULT_ANTALL}")
+    private int DEFAULT_ANTALL;
+
     @Timed(value = "get.counter.requests")
     @RequestMapping(path = "hendelser", method = RequestMethod.GET)
     public Feed alleHendelser(HttpServletRequest request,
-                              @RequestParam(value="side", defaultValue=DEFAULT_SIDE) String sideInt,
-                              @RequestParam(value="antall", defaultValue=DEFAULT_ANTALL) String antallInt)
-            throws BadParameterException {
-
-        var side = convertToInt(sideInt, "side");
-        var antall = convertToInt(antallInt, "antall");
-
-        if(antall>MAX_ANTALL) {
+                              @RequestParam(value = "antall", required = false) Integer antall,
+                              @RequestParam(value = "side", required = false) Integer side) throws BadParameterException {
+        if (side == null) side = DEFAULT_SIDE;
+        if (antall == null) antall = DEFAULT_ANTALL;
+        if (antall > MAX_ANTALL) {
             throw new BadParameterException("Man kan ikke be om flere enn " + MAX_ANTALL + " hendelser.");
         }
 
@@ -49,10 +47,9 @@ public class FeedController {
         var hendelser = database.fetch(side, antall);
         feed.setHendelser(hendelser.stream().map(Mapper::map).collect(Collectors.toList()));
 
-        if (side < database.getNumberOfPages()) {
-            String sideParam = request.getQueryString().split("&")[0];
-            feed.setNext_url(request.getRequestURL().toString() + "?" +
-                    request.getQueryString().replace(sideParam, "side=" + (side + 1)));
+        if (side < database.getNumberOfPages(antall)) {
+            feed.setNext_url(request.getRequestURL().toString() +
+                String.format("?side=%d&andtall=%d", side + 1, antall));
         }
 
         return feed;
--- src/main/java/no/nav/samordning/hendelser/hendelse/Database.java
+++ src/main/java/no/nav/samordning/hendelser/hendelse/Database.java
@@ -1,75 +1,35 @@
 package no.nav.samordning.hendelser.hendelse;
 
-import org.apache.tomcat.jni.Local;
 import org.postgresql.util.PGobject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
-import java.sql.SQLException;
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
 public class Database {
-    private String SQL_FETCH = "SELECT * \n" +
-        "FROM T_SAMORDNINGSPLIKTIG_VEDTAK \n" +
-        "WHERE (ctid::text::point)[0]::int = ? " +
-        "LIMIT ?" +
-        "";
-
-    private static final String SQL_INSERT_RECORD = "INSERT INTO T_SAMORDNINGSPLIKTIG_VEDTAK VALUES('?')";
-
+    private final String SQL_FETCH = "SELECT HENDELSE_DATA FROM HENDELSER OFFSET ? LIMIT ?";
+    private final String SQL_TOTAL_COUNT = "SELECT COUNT(HENDELSE_DATA) FROM HENDELSER";
     private JdbcTemplate database;
 
-    @Autowired
     public Database(JdbcTemplate database) {
         this.database = database;
     }
 
-    public List<Hendelse> fetch(Integer side,
-                                Integer antall) {
-
-        List<Hendelse> hendelser = new ArrayList<>();
-
-        List<PGobject> jsonHendelser = database.queryForList(
-                SQL_FETCH,
-                PGobject.class,
-                side,
-                antall);
-
-        Jsonb jsonb = JsonbBuilder.create();
-
-        for (PGobject jsonHendelse : jsonHendelser) {
-            System.out.println("VALUE: " + jsonHendelse.getValue());
-            hendelser.add(jsonb.fromJson(jsonHendelse.getValue(), Hendelse.class));
-        }
-
-        return hendelser;
+    public List<Hendelse> fetch(int side, int antall) {
+        return database.queryForList(SQL_FETCH, PGobject.class, side * antall, antall)
+            .stream().map(hendelse -> JsonbBuilder.create().fromJson(hendelse.getValue(), Hendelse.class))
+            .collect(Collectors.toList());
     }
 
-    public void insert(Hendelse hendelse) throws SQLException {
-        Jsonb jsonb = JsonbBuilder.create();
-        PGobject data = new PGobject();
-        data.setType("jsonb");
-        data.setValue(jsonb.toJson(hendelse));
-
-        database.update(SQL_INSERT_RECORD, data);
-    }
-
-    public int getNumberOfPages() {
-        var SQL_GET_NUMBER_OF_PAGES = "SELECT MAX((ctid::text::point)[0]::int) FROM T_SAMORDNINGSPLIKTIG_VEDTAK";
-        int numberOfPages;
-
+    public int getNumberOfPages(int antall) {
+        int numberOfPages = 0;
         try {
-            numberOfPages = Integer.parseInt(database.queryForObject(SQL_GET_NUMBER_OF_PAGES, String.class));
-        } catch (NumberFormatException e) {
-            return 0;
-        }
-
+            var total = Integer.parseInt(database.queryForObject(SQL_TOTAL_COUNT, String.class));
+            numberOfPages = (total + antall - 1) / antall;
+        } catch (Exception e) {}
         return numberOfPages;
     }
 }
