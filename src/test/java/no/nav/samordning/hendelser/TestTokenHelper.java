a8aa9466d5d1e121dd1f204ec2190362a125a450
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2019-08-07 10:44:57 +0200

    Fix warnings and clean TestTokenHelper.

--- src/test/java/no/nav/samordning/hendelser/TestTokenHelper.java
+++ src/test/java/no/nav/samordning/hendelser/TestTokenHelper.java
@@ -2,6 +2,7 @@ package no.nav.samordning.hendelser;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import org.jetbrains.annotations.NotNull;
 
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
@@ -12,55 +13,78 @@ import java.util.Base64;
 
 public class TestTokenHelper {
 
+    private static final String AUTH_SCHEME = "Bearer";
     private static KeyPair keyPair;
 
-    public static String generateJwks() throws NoSuchAlgorithmException {
-        keyPair = generateKeyPair();
-        return String.format("{\n" +
-            "  \"keys\": [{\n" +
-            "      \"kty\": \"RSA\",\n" +
-            "      \"e\": \"AQAB\",\n" +
-            "      \"use\": \"sig\",\n" +
-            "      \"n\": \"%s\"\n" +
-            "    }\n" +
-            "  ]\n" +
-            "}", Base64.getUrlEncoder().encodeToString(((RSAPublicKey)keyPair.getPublic()).getModulus().toByteArray()));
-    }
-
-    public static String token(String orgno, Boolean verifiedSignature) throws NoSuchAlgorithmException {
-        var signingKeys = keyPair;
-
-        if (!verifiedSignature) {
-            signingKeys = generateKeyPair();
-        }
-
-        var algorithm = Algorithm.RSA256((RSAPublicKey) signingKeys.getPublic(), (RSAPrivateKey) signingKeys.getPrivate());
-        var builder = JWT.create();
-        builder.withArrayClaim("aud", new String[]{"tp_ordning", "preprod"});
-        builder.withClaim("iss", "https://badserver/provider/");
-        builder.withClaim("scope", "nav:samordning/v1/hendelser");
-        builder.withClaim("client_id", "tp_ordning");
-        builder.withClaim("client_orgno", orgno);
-        return "Bearer " + builder.sign(algorithm);
+    public static String token(String orgno, boolean verifiedSignature) throws NoSuchAlgorithmException {
+        var signingKeys = verifiedSignature ? keyPair : generateKeyPair();
+        var algorithm = Algorithm.RSA256(publicKey(signingKeys), privateKey(signingKeys));
+        return header(createJwt(orgno, algorithm));
     }
 
     public static String srvToken() {
-        var algorithm = Algorithm.RSA256((RSAPublicKey) keyPair.getPublic(), (RSAPrivateKey) keyPair.getPrivate());
-        var builder = JWT.create();
-        builder.withClaim("azp", "srvTest");
-        builder.withClaim("iss", "test");
-        return "Bearer " + builder.sign(algorithm);
+        return header(createSrvJwt(algorithm()));
     }
 
     public static String emptyToken() {
-        var algorithm = Algorithm.RSA256((RSAPublicKey) keyPair.getPublic(), (RSAPrivateKey) keyPair.getPrivate());
-        var builder = JWT.create();
-        return "Bearer " + builder.sign(algorithm);
+        return header(JWT.create().sign(algorithm()));
+    }
+
+    static String generateJwks() throws NoSuchAlgorithmException {
+        keyPair = generateKeyPair();
+
+        return String.format("{\n" +
+                "  \"keys\": [{\n" +
+                "      \"kty\": \"RSA\",\n" +
+                "      \"e\": \"AQAB\",\n" +
+                "      \"use\": \"sig\",\n" +
+                "      \"n\": \"%s\"\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}", Base64.getUrlEncoder().encodeToString(getKeyModulus()));
     }
 
     private static KeyPair generateKeyPair() throws NoSuchAlgorithmException {
-        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-        keyPairGenerator.initialize(1024);
-        return keyPairGenerator.generateKeyPair();
+        var generator = KeyPairGenerator.getInstance("RSA");
+        generator.initialize(1024);
+        return generator.generateKeyPair();
+    }
+
+    private static String createJwt(String orgno, Algorithm algorithm) {
+        return JWT.create()
+                .withArrayClaim("aud", new String[]{"tp_ordning", "preprod"})
+                .withClaim("iss", "https://badserver/provider/")
+                .withClaim("scope", "nav:samordning/v1/hendelser")
+                .withClaim("client_id", "tp_ordning")
+                .withClaim("client_orgno", orgno)
+                .sign(algorithm);
+    }
+
+    private static String createSrvJwt(Algorithm algorithm) {
+        return JWT.create()
+                .withClaim("azp", "srvTest")
+                .withClaim("iss", "test")
+                .sign(algorithm);
+    }
+
+    @NotNull
+    private static Algorithm algorithm() {
+        return Algorithm.RSA256(publicKey(keyPair), privateKey(keyPair));
+    }
+
+    private static RSAPrivateKey privateKey(KeyPair keyPair) {
+        return (RSAPrivateKey) keyPair.getPrivate();
+    }
+
+    private static RSAPublicKey publicKey(KeyPair keyPair) {
+        return (RSAPublicKey) keyPair.getPublic();
+    }
+
+    private static byte[] getKeyModulus() {
+        return publicKey(keyPair).getModulus().toByteArray();
+    }
+
+    private static String header(String token) {
+        return AUTH_SCHEME + " " + token;
     }
 }
