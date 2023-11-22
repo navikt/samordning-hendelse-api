bc9b459bbb0f2de2001d006eb33fd44ffa998471
K153294 <martin.knutsen@nav.no>
2023-11-22 11:59:30 +0100

    PES-407: EnableKafka
    Co-authored-by: Jens Chr. Madsen <Jens.Christian.Madsen@nav.no>

--- src/main/kotlin/no/nav/samordning/hendelser/Application.kt
+++ src/main/kotlin/no/nav/samordning/hendelser/Application.kt
@@ -2,8 +2,10 @@ package no.nav.samordning.hendelser
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.kafka.annotation.EnableKafka
 
 @SpringBootApplication
+@EnableKafka
 class Application
 
 fun main(args: Array<String>) {
