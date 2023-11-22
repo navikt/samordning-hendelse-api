8df7e3c636543c4c97a2f697b50274b984bbf0ae
Jens Chr. Madsen <jens.christian.madsen@nav.no>
2023-11-22 13:48:13 +0100

    PES-407: forbedring logging. fiks på bygg og deploy

--- src/main/kotlin/no/nav/samordning/hendelser/kafka/VarsleVedtakSamordningListener.kt
+++ src/main/kotlin/no/nav/samordning/hendelser/kafka/VarsleVedtakSamordningListener.kt
@@ -5,7 +5,7 @@ import no.nav.samordning.hendelser.hendelse.HendelseContainer
 import no.nav.samordning.hendelser.hendelse.HendelseRepository
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.slf4j.Logger
-import org.slf4j.LoggerFactory
+import org.slf4j.LoggerFactory.getLogger
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Service
@@ -16,18 +16,20 @@ import org.springframework.transaction.annotation.Transactional
 class VarsleVedtakSamordningListener(
     private val hendelseRepository: HendelseRepository
 ) {
-    private val LOG: Logger = LoggerFactory.getLogger(javaClass)
+    private val LOG: Logger = getLogger(javaClass)
 
     @KafkaListener(topics = ["\${VEDTAK_HENDELSE_KAFKA_TOPIC}"])
     fun listener(hendelse: SamHendelse, cr: ConsumerRecord<String, Hendelse>, acknowledgment: Acknowledgment) {
-        LOG.debug("*** Innkommende VedtakHendelse. Offset: ${cr.offset()}, Partition: ${cr.partition()}, Key: ${cr.key()} ${if (LOG.isDebugEnabled) ", VedtakHendelse: $hendelse" else ""} ")
+        LOG.info("*** Innkommende VedtakHendelse. Offset: ${cr.offset()}, Partition: ${cr.partition()}, Key: ${cr.key()}")
+        //if (LOG.isDebugEnabled) LOG.debug("VedtakHendelse: $hendelse")
 
         try {
+
             hendelseRepository.saveAndFlush(
                 HendelseContainer(hendelse)
             )
             acknowledgment.acknowledge()
-            LOG.debug("*** Acket melding ferdig")
+            LOG.info("*** Acket melding ferdig")
 
         } catch (e: Exception) {
             LOG.error("Feilet ved lagre VedtakHendelse, melding: ${e.message}", e)
