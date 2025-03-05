decfc0f378630a6e0f0367ddd0104a0b3fb913cf
Jens Chr. Madsen <jens.christian.madsen@nav.no>
2025-03-05 14:05:46 +0100

    bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,9 +14,9 @@ val mockkVersion = "1.13.12"
 
 plugins {
     kotlin("jvm") version "2.1.0"
-    kotlin("plugin.spring") version "2.0.21"
+    kotlin("plugin.spring") version "2.1.0"
     kotlin("plugin.jpa") version "2.1.0"
-    id("org.springframework.boot") version "3.4.0"
+    id("org.springframework.boot") version "3.4.1"
     id("io.spring.dependency-management") version "1.1.7"
 }
 
