cc15342e419b7a151dce2d51ee2c081f2443ae93
Morten Normann <morten.normann@nav.no>
2026-02-02 14:11:01 +0100

    Legg til flere tester av PersonEndringListener
    Co-authored-by: Jens Chr. Madsen <jens.christian.madsen@nav.no>

--- src/main/kotlin/no/nav/samordning/hendelser/person/kafka/PersonEndringListener.kt
+++ src/main/kotlin/no/nav/samordning/hendelser/person/kafka/PersonEndringListener.kt
@@ -58,13 +58,14 @@ class PersonEndringListener(
             }
 
         } catch (e: Exception) {
+            // TODO: Finn ut om hendelse burde ackes
             acknowledgment.acknowledge()
             logger.error("Feilet ved deserializering, Acket, melding må sendes på nytt, melding: ${e.message}", e)
             return
         }
 
         try {
-            //hvis døsfall finnes.. på fnr.. fra så sletter recod..
+            // TODO: Burde vi håndtere annullering/opphør (av dødsfall)?
             val personEndring = personEndringHendelser.map { personEndring ->
                 val sisteSekvensnummer = personEndringRepository.getFirstByTpnrOrderBySekvensnummerDesc(personEndring.tpnr) ?.sekvensnummer ?: 0
                 personEndring.sekvensnummer = sisteSekvensnummer + 1
--- src/test/kotlin/no/nav/samordning/hendelser/person/PersonEndringListenerTest.kt
+++ src/test/kotlin/no/nav/samordning/hendelser/person/PersonEndringListenerTest.kt
@@ -6,6 +6,7 @@ import ch.qos.logback.core.read.ListAppender
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
+import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import io.zonky.test.db.AutoConfigureEmbeddedDatabase
@@ -17,12 +18,15 @@ import no.nav.samordning.hendelser.person.repository.PersonHendelseRepository
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.slf4j.LoggerFactory
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.kafka.support.Acknowledgment
+import java.io.IOException
 import java.time.LocalDate
+import java.util.UUID
 import kotlin.test.assertEquals
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -44,9 +48,6 @@ class PersonEndringListenerTest {
     @Autowired
     private lateinit var personHendelseRepository: PersonHendelseRepository
 
-    //private val personEndringHendelseJson = mapper.writeValueAsString(mockPersonEndringKafkaHendelse())
-    //private val personEndringHendelseJson1 = mapper.writeValueAsString(mockPersonEndringKafkaHendelse("3020"))
-
     @BeforeEach
     fun setup() {
         listAppender.start()
@@ -59,44 +60,64 @@ class PersonEndringListenerTest {
     }
 
     @Test
-    fun testingAvConsumer() {
-        val personEndringHendelseSPK3010 =  mockPersonEndringKafkaHendelse(tpnr = "3010")
-        val personEndringHendelseJson = mapper.writeValueAsString(personEndringHendelseSPK3010)
+    fun `test av hendelse med et tpnr i seg`() {
+        val personEndringHendelse =  mockPersonEndringKafkaHendelse(tpnr = listOf("3090"))
+        val personEndringHendelseJson = mapper.writeValueAsString(personEndringHendelse)
 
         listener.listener(personEndringHendelseJson, mockk(relaxed = true), acknowledgment)
 
-        assertEquals(1, personEndringRepository.countAllByTpnr("3010"))
-        assertEquals(true, personHendelseRepository.existsByHendelseIdAndMeldingskode(personEndringHendelseSPK3010.hendelseId, personEndringHendelseSPK3010.meldingsKode))
+        assertEquals(1, personEndringRepository.countAllByTpnr("3090"))
+        assertEquals(true, personHendelseRepository.existsByHendelseIdAndMeldingskode(personEndringHendelse.hendelseId, personEndringHendelse.meldingsKode))
 
         //test på skal ikke behandles på nytt. (samme hendelseid)
         listener.listener(personEndringHendelseJson, mockk(relaxed = true), acknowledgment)
-        assertEquals(1, personEndringRepository.countAllByTpnr("3010"))
-
+        assertEquals(1, personEndringRepository.countAllByTpnr("3090"))
 
         verify(exactly = 2) { acknowledgment.acknowledge() }
     }
 
-//    @Test
-//    fun testingAvConsumerFeilerVedLagring() {
-//        val ytelseHendelserRepository = mockk<YtelseHendelserRepository>(relaxed = true)
-//        val listener2 = VarsleEndringTPYtelseListener(ytelseHendelserRepository)
-//        every { ytelseHendelserRepository.save(any()) } returnsArgument 0
-//        every { ytelseHendelserRepository.flush() } throws IOException("IO error")
-//
-//        assertThrows<IOException> {
-//            listener2.listener(hendelseJson, mockk(relaxed = true), acknowledgment)
-//        }
-//
-//        verify(exactly = 0) { acknowledgment.acknowledge()  }
-//        verify(exactly = 2) {
-//            ytelseHendelserRepository.save(any()) }
-//        verify(exactly = 1) { ytelseHendelserRepository.flush() }
-//    }
+    @Test
+    fun `test av hendelser med flere tpnr i seg`() {
+        val personEndringHendelser1 =  mockPersonEndringKafkaHendelse(tpnr = listOf("3010", "3200", "3400"))
+        val personEndringHendelser2 =  mockPersonEndringKafkaHendelse(tpnr = listOf("3200", "3400"))
+        val personEndringHendelse1Json = mapper.writeValueAsString(personEndringHendelser1)
+        val personEndringHendelse2Json = mapper.writeValueAsString(personEndringHendelser2)
+
+        listener.listener(personEndringHendelse1Json, mockk(relaxed = true), acknowledgment)
+        listener.listener(personEndringHendelse2Json, mockk(relaxed = true), acknowledgment)
+
+        assertEquals(1, personEndringRepository.countAllByTpnr("3010"))
+        assertEquals(2, personEndringRepository.countAllByTpnr("3200"))
+        assertEquals(2, personEndringRepository.countAllByTpnr("3400"))
+        assertEquals(true, personHendelseRepository.existsByHendelseIdAndMeldingskode(personEndringHendelser1.hendelseId, personEndringHendelser1.meldingsKode))
+        assertEquals(true, personHendelseRepository.existsByHendelseIdAndMeldingskode(personEndringHendelser2.hendelseId, personEndringHendelser2.meldingsKode))
+
+        verify(exactly = 2) { acknowledgment.acknowledge() }
+    }
+
+    @Test
+    fun `verifiser at hendelsen ikke ackes ved database-feil`() {
+        val personEndringHendelse =  mockPersonEndringKafkaHendelse(tpnr = listOf("3010"))
+        val personEndringHendelseJson = mapper.writeValueAsString(personEndringHendelse)
+
+        val personEndringRepository = mockk<PersonEndringRepository>(relaxed = true)
+        val personEndringListener = PersonEndringListener(personEndringRepository, mockk(relaxed = true))
+        every { personEndringRepository.save(any()) } returnsArgument 0
+        every { personEndringRepository.flush() } throws IOException("IO error")
+
+        assertThrows<IOException> {
+            personEndringListener.listener(personEndringHendelseJson, mockk(relaxed = true), acknowledgment)
+        }
+
+        verify(exactly = 0) { acknowledgment.acknowledge()  }
+        verify(exactly = 1) { personEndringRepository.save(any()) }
+        verify(exactly = 1) { personEndringRepository.flush() }
+    }
 
 
-    fun mockPersonEndringKafkaHendelse(tpnr: String, fnr: String = "1001010101") = PersonEndringKafkaHendelse(
-        hendelseId = "1",
-        tpNr = listOf(tpnr),
+    fun mockPersonEndringKafkaHendelse(tpnr: List<String>, fnr: String = "1001010101") = PersonEndringKafkaHendelse(
+        hendelseId = UUID.randomUUID().toString(),
+        tpNr = tpnr,
         fnr = fnr,
         sivilstand = "GIFT",
         sivilstandDato = LocalDate.now(),
